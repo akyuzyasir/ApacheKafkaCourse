e396a57a9e50d81d39712dd735b4ea061da42b2d
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-24 16:41:05 +0100

    Kafka Service is created. CreateTopicAsync method is created.

---
+++ KafkaComponents/P08TopicCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaComponents
+{
+    internal class P08TopicCreate
+    {
+        // How do we create a topic in Kafka?
+        // First we use Confluent.Kafka NuGet package
+        // Partition Count, ISR Count and Replication Factor are the key parameters
+        // Read and write processes only happen in the leader partition
+    }
+}
