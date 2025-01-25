921a56b47f34eb94329e4418b59fad3dce35a9ac
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-25 15:54:39 +0100

    Use Case - 2 MessageWithIntKey New Methods Created to Produce and Consume Messages with Int Key

--- Kafka.Producer/Program.cs
+++ Kafka.Producer/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Kafka Producer");
 
 
 var kafkaService = new KafkaProducerService();
-var topicName = "use-case-1.1-topic";
+var topicName = "use-case-2-topic";
 await kafkaService.CreateTopicAsync(topicName);
-await kafkaService.SendSimpleMessageWithNullKey(topicName);
+await kafkaService.SendSimpleMessageWithIntKey(topicName);
 
