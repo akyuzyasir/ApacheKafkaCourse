7a16bc12f20197c90d66ff0d1802e7553e50643d
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-25 15:09:54 +0100

    Multiple Consumer Group Secon Consumer Group is created. Null checks added for topicName

--- Kafka.Consumer/Program.cs
+++ Kafka.Consumer/Program.cs
@@ -2,10 +2,18 @@
 using Kafka.Consumer;
 
 Console.WriteLine("Kafka Consumer 1");
-var topicName = "use-case-1-topic";
-var kafkaService = new KafkaService();
+// We added try-catch block to catch the exception if the topicName is null, empty, or whitespace.
+try
+{
+    var topicName = "use-case-1.1-topic";
+    var kafkaService = new KafkaConsumerService();
 
-// we have 3 partitions and 1 replication factor for the topic. So, we can run 3 consumers at the same time and each consumer will consume messages from a different partition. But if we run more than 3 consumers, some of them will be idle because we have only 3 partitions.
-await kafkaService.ConsumeSimpleMessageWithNullKey(topicName);
+    // we have 3 partitions and 1 replication factor for the topic. So, we can run 3 consumers at the same time and each consumer will consume messages from a different partition. But if we run more than 3 consumers, some of them will be idle because we have only 3 partitions.
+    await kafkaService.ConsumeSimpleMessageWithNullKey(topicName);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
 
 Console.ReadLine(); // To keep the console open
