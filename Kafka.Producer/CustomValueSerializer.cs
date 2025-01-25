a0aa7880e997ee76eb54decfd4c66e81955f1e7e
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-25 17:23:13 +0100

    Use Case - 3 Serialize-Deserialize
    Add complex message support with custom serializer
    
    Introduced SendComplexMessageWithIntKey method in KafkaProducerService to send complex messages with an integer key. Updated Program.cs to use this new method and changed the topic name to "use-case-3-topic". Added CustomValueSerializer<T> for JSON serialization of messages. Introduced OrderCreatedEvent record to represent event data.

---
+++ Kafka.Producer/CustomValueSerializer.cs
@@ -0,0 +1,19 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Kafka.Producer
+{
+    internal class CustomValueSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+        {
+            // We serialize T type data to JSON format and then convert it to a byte array.
+            return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(data, typeof(T)));
+        }
+    }
+}
